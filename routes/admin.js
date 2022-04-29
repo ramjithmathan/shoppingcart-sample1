47acb8a003697381dcec722a215ff6721d5f9300
Ramjith Mathan <ramjith.mn@gmail.com>
2022-04-28 21:05:07 -1200

    1st commit

---
+++ routes/admin.js
@@ -0,0 +1,31 @@
+var express = require('express');
+const productHelpers = require('../helper/product-helpers');
+var router = express.Router();
+var productHelper=require('../helper/product-helpers')
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  productHelpers.getAllProducts().then((products)=>{
+    console.log(products);
+    res.render('admin/view-products',{admin:true,products})
+  })
+  
+});
+router.get('/add-product',function(req,res){
+   res.render('admin/add-product',{admin:true})
+})
+router.post('/add-product',(req,res)=>{
+   productHelpers.addProduct(req.body,(id)=>{
+     let image=req.files.Image
+     console.log(id);
+     image.mv('./public/product-images/'+id+'.jpg',(err,done)=>{
+       if(!err){
+        res.render("admin/add-product")
+       }else{
+         console.log(err)
+       }
+     })
+     
+   })
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+const productHelpers = require('../helper/product-helpers');
+var router = express.Router();
+var productHelper=require('../helper/product-helpers')
+const userHelpers=require('../helper/user-helpers')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let user=req.session.user
+  console.log(user)
+  productHelpers.getAllProducts().then((products)=>{
+    res.render('user/view-products',{products,user})
+  })
+  
+});
+router.get('/login',(req,res)=>{
+  res.render('user/login')
+})
+router.get('/signup',(req,res)=>{ 
+  res.render('user/signup')
+})
+router.post('/signup',(req,res)=>{
+  userHelpers.doSignup(req.body).then((response)=>{
+    console.log(response)
+  })
+})
+router.post('/login',(req,res)=>{
+  userHelpers.doLogin(req.body).then((response)=>{
+    if(response.status){
+      req.session.loggedIn=true
+      req.session.user=response.user
+      res.redirect('/')
+    }else{
+      res.redirect('/login')
+    }
+  })
+})
+router.get('/logout',(req,res)=>{
+  req.session.destroy()
+  res.redirect('/')
+})
+
+module.exports = router;
